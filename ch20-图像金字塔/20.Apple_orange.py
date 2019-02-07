d1d9fd4fc15ad6745b3d90ffe2d6771e9506a177
Mr. Mino <40648362+MasterKino@users.noreply.github.com>
2019-02-07 19:09:22 +0800

    Update and rename ch20-图像金字塔/20.Apple_orange.py to  ch20-图像金字塔/20.Apple_orange.py

--- ch20-图像金字塔/20.Apple_orange.py
+++ ch20-图像金字塔/20.Apple_orange.py
@@ -10,61 +10,68 @@
 3. 根据 斯 字塔 算拉普拉斯 字塔
 4. 在拉普拉斯的每一层  图像 合 苹果的左 与橘子的右  合  5. 根据 合后的图像 字塔 建原始图像。
 '''
-
 import cv2
-import numpy as np, sys
+import numpy as np
 
 A = cv2.imread('apple.jpg')
 B = cv2.imread('orange.jpg')
-
+print(type(A))
 # generate Gaussian pyramid for A
 G = A.copy()
+print(G)
 gpA = [G]
 for i in range(6):
     G = cv2.pyrDown(G)
     gpA.append(G)
-
-# generate Gaussian pyramid for B
 G = B.copy()
 gpB = [G]
 for i in range(6):
     G = cv2.pyrDown(G)
     gpB.append(G)
-
-# generate Laplacian Pyramid for A
 lpA = [gpA[5]]
-for i in range(5, 0, -1):
+for i in range(6, 0, -1):
+    print(i)
     GE = cv2.pyrUp(gpA[i])
-    L = cv2.subtract(gpA[i - 1], GE)#TODO error
+    GE = cv2.resize(GE, gpA[i - 1].shape[-2::-1])
+    L = cv2.subtract(gpA[i-1], GE)
+    print(L.shape)
     lpA.append(L)
-    #cv2.error: /Users/play/Temp/opencv/modules/core/src/arithm.cpp:659: error: (-209) The operation is neither 'array op array' (where arrays have the same size and the same number of channels), nor 'array op scalar', nor 'scalar op array' in function arithm_op
-
-
 # generate Laplacian Pyramid for B
 lpB = [gpB[5]]
-for i in range(5, 0, -1):
+for i in range(6, 0, -1):
+    print(i)
     GE = cv2.pyrUp(gpB[i])
-    L = cv2.subtract(gpB[i - 1], GE)
+    GE = cv2.resize(GE, gpB[i - 1].shape[-2::-1])
+    L = cv2.subtract(gpB[i-1], GE)
+    print(L.shape)
     lpB.append(L)
-
 # Now add left and right halves of images in each level
-# numpy.hstack(tup)
-# Take a sequence of arrays and stack them horizontally
-# to make a single array.
 LS = []
-for la, lb in zip(lpA, lpB):
+lpAc = []
+for i in range(len(lpA)):
+    b = cv2.resize(lpA[i], lpB[i].shape[-2::-1])
+    print(b.shape)
+    lpAc.append(b)
+print(len(lpAc))
+print(len(lpB))
+j = 0
+for i in zip(lpAc, lpB):
+    print(i)
+    print('ss')
+    la, lb = i
+    print(la)
+    print(lb)
     rows, cols, dpt = la.shape
-    ls = np.hstack((la[:, 0:cols / 2], lb[:, cols / 2:]))
+    ls = np.hstack((la[:, 0:cols//2], lb[:, cols//2:]))
+    j = j+1
+    print(j)
     LS.append(ls)
-
-# now reconstruct
 ls_ = LS[0]
 for i in range(1, 6):
     ls_ = cv2.pyrUp(ls_)
+    ls_ = cv2.resize(ls_, LS[i].shape[-2::-1])
     ls_ = cv2.add(ls_, LS[i])
-
 # image with direct connecting each half
-real = np.hstack((A[:, :cols / 2], B[:, cols / 2:]))
+B = cv2.resize(B, A.shape[-2::-1])
+real = np.hstack((A[:, :cols//2], B[:, cols//2:]))
 
-cv2.imwrite('Pyramid_blending2.jpg', ls_)
-cv2.imwrite('Direct_blending.jpg', real)
